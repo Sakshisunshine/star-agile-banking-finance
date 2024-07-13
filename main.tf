795edeb87be2c4dd1e8c126cc35c23c3d5a3260e
Sakshisunshine <sunshine.chouhan1@gmail.com>
2024-07-14 01:54:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -131,10 +131,10 @@ resource "aws_eip" "proj-eip" {
 
 # Creating an ubuntu EC2 instance
 resource "aws_instance" "Prod-Server" {
-  ami             = "ami-0ef82eeba2c7a0eeb"  # Verify this AMI ID for us-east-1
+  ami             = "ami-0a0e5d9c7acc336f1"  # Verify this AMI ID for us-east-1
   instance_type   = "t2.micro"
   availability_zone = "us-east-1a"  # Update to us-east-1a
-  key_name        = "shiva-mentor"
+  key_name        = "Key-Pair-Jenkins"
   
   network_interface {
     device_index            = 0
